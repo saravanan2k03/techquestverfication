1781838217830b0b3feb98f26352e7bc56113599
saravanan2k03 <sarothehacker@gmail.com>
2023-09-09 08:11:50 +0530

    add

--- lib/views/Entry.dart
+++ lib/views/Entry.dart
@@ -5,7 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
-
 import 'Detail.dart';
 
 class Entry extends StatefulWidget {
